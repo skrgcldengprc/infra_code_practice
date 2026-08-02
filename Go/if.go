149b1fded4938ad004b6bd2d1987557cef49a556
skrg <wpqx95kfnj@privaterelay.appleid.com>
2026-08-02 23:30:23 +0900

    second

--- Go/if.go
+++ Go/if.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	a := 4
+	a := 5
 	if a > 5 {
 		fmt.Println("aは5より大きいです。")
 	} else if a < 5 {
 		fmt.Println("aは5より小さいです。")
+	} else {
+		fmt.Println("aは5と等しいです。")
 	}
 }
