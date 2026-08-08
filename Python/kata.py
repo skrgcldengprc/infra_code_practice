76a9ddab9afa42f26660a5b5eb341a3e83e5d640
skrg <wpqx95kfnj@privaterelay.appleid.com>
2026-08-08 23:42:35 +0900

    maze

---
+++ Python/kata.py
@@ -0,0 +1,24 @@
+# データ型
+
+# str 文字列
+# int 小数点を含まない整数
+# float 少数
+# bool 真偽値 True or False
+# list 配列 []の中に複数の要素を[,]で区切って格納する
+# tuple 配列と似ているが、要素の追加や削除ができない ()の中に複数の要素を[,]で区切って格納する
+# dict 辞書型 {}の中に複数の要素を[,]で区切って格納する。キーと値(Key:Value)のペアで構成される
+
+# 整数
+print(type(100))
+# 少数
+print(type(100.1))
+# 文字列
+print(type("Hello"))
+# 真偽値
+print(type(True))
+# 配列
+print(type([1, 2, 3]))
+# タプル
+print(type((1, 2, 3)))
+# 辞書型
+print(type({"name": "Yuya", "age": 30}))
